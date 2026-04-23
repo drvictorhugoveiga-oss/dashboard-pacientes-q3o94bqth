38f06d0c9d6301b79b343d1eaa45fb5ffea6e62e
Skip <skip@usecurling.com>
2026-04-23 02:20:54 +0000

    feat: Implement Sessions by Professional page

--- src/services/pacientes.ts
+++ src/services/pacientes.ts
@@ -1,3 +1,10 @@
 import pb from '@/lib/pocketbase/client'
 
 export const getPacientes = () => pb.collection('pacientes').getFullList({ sort: 'nome' })
+
+export const getPacienteCompleto = (id: string) => pb.collection('pacientes').getOne(id)
+
+export const createPacienteCompleto = (data: any) => pb.collection('pacientes').create(data)
+
+export const updatePacienteCompleto = (id: string, data: any) =>
+  pb.collection('pacientes').update(id, data)
